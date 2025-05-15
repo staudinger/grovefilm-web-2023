f4cd2aa0c5bccdebd7ce5b25f3cb6f8c629a8b7c
staudinger <albert4lbertalbert@gmail.com>
2025-05-15 10:18:20 -0400

    update fade

--- components/FadeText.tsx
+++ components/FadeText.tsx
@@ -1,17 +1,8 @@
 import React, { useState, useEffect } from "react";
 // import './fade.css';
 
-const FadeText = ({ text, duration = 1000 }: any) => {
-	const [visible, setVisible] = useState(false);
-
-	useEffect(() => {
-		setVisible(false); // Fade in
-		const timer = setTimeout(() => setVisible(true), duration); // Fade out after `duration`
-
-		return () => clearTimeout(timer);
-	}, [text, duration]);
-
-	return <div className={`fade-text ${visible ? "show" : ""}`}>{text}</div>;
+const FadeText = ({ text }: any) => {
+	return <div className="fade-loop">{text}</div>;
 };
 
 export default FadeText;
